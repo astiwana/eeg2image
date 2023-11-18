677e21d25590103d695fba44e22b1470ddba4c2a
Tony <ruotongs@outlook.com>
2023-11-18 13:49:37 -0700

    first

---
+++ visualization.py
@@ -0,0 +1,34 @@
+import numpy as np
+
+from sklearn.cluster import KMeans
+from scipy.optimize import linear_sum_assignment as linear_assignment
+
+class K_means:
+    def __init__(self, n_clusters=40, random_state=45):
+        self.n_clusters = n_clusters
+        self.random_state = random_state
+        
+    def transform(self, embed, gt_labels):
+        pred_labels = KMeans(n_clusters=self.n_clusters, n_init=10, random_state=self.random_state).fit_predict(embed)
+        score       = self.cluster_acc(gt_labels, pred_labels)
+        # image_score = K_means_model.score(image_embed, KMeans(n_clusters=self.n_clusters, random_state=self.random_state).fit_predict(image_embed))
+        return score
+
+    # Thanks to: https://github.com/k-han/DTC/blob/master/utils/util.py
+    def cluster_acc(self, y_true, y_pred):
+        """
+        Calculate clustering accuracy. Require scikit-learn installed
+        # Arguments
+            y: true labels, numpy.array with shape `(n_samples,)`
+            y_pred: predicted labels, numpy.array with shape `(n_samples,)`
+        # Return
+            accuracy, in [0,1]
+        """
+        y_true = y_true.astype(np.int64)
+        assert y_pred.size == y_true.size
+        D = max(y_pred.max(), y_true.max()) + 1
+        w = np.zeros((D, D), dtype=np.int64)
+        for i in range(y_pred.size):
+            w[y_pred[i], y_true[i]] += 1
+        ind = linear_assignment(w.max() - w)
+        return sum([w[i, j] for i, j in zip(*ind)]) * 1.0 / y_pred.size
